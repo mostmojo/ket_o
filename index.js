b5a812f8dd41132ba05866905a82f486374c5793
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-02-21 13:29:10 +0200

    Implement startTimer

--- index.js
+++ index.js
@@ -24,25 +24,25 @@
 
 // /* ----------- Timer ----------- */
 
-// function startTimer(duration, display) {
-//     var timer = duration,
-//         minutes, seconds;
-//     setInterval(function() {
-//         minutes = parseInt(timer / 60, 10);
-//         seconds = parseInt(timer % 60, 10);
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-//         display.textContent = minutes + ":" + seconds;
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
-// }
-// window.onload = function() {
-//     var tenMinutes = 60 * 10,
-//         display = document.querySelector('#stopwatch');
-//     startTimer(tenMinutes, display);
-// };
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function() {
+    var tenMinutes = 60 * 10,
+        display = document.querySelector('#stopwatch');
+    startTimer(tenMinutes, display);
+};
 
 // /* ---------- Country state -----------*/
 // <script type="text/javascript">
