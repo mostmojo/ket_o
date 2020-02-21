6a7914c85a5eb09d827abbc3e8728e0249f14a73
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-02-21 14:58:50 +0200

    Fix JS for timer

--- index.js
+++ index.js
@@ -23,6 +23,31 @@
 
 // /* ----------- Timer to boot the 10 minute countdown ----------- */
 
+function timerBoot(duration, display) {
+	let timer = duration;
+	let minutes;
+	let seconds;
+	setInterval(() => {
+			minutes = parseInt(timer / 60, 10);
+			seconds = parseInt(timer % 60, 10);
+			minutes = minutes < 10 ? `0${minutes}` : minutes;
+			seconds = seconds < 10 ? `0${seconds}` : seconds;
+			display.textContent = `${minutes}:${seconds}`;
+			if (--timer < 0) {
+					timer = duration;
+			}
+	}, 1000);
+}
+window.onload = () => {
+	const tenMinutes = 60 * 10;
+	const display = document.querySelector('#countdown');
+	timerBoot(tenMinutes, display);
+};
+
+
+ /*---------- Countries ----------*/
+
+
 // $(document).ready(function() {
 //   initializeCountryState(
 //     "country",
@@ -45,7 +70,6 @@
 //   });
 // });
 
-// /*---------- Countries ----------*/
 // function initializeCountryState(
 //   country_id,
 //   state_id,
@@ -5201,4 +5225,3 @@
 
 //   country_element.change();
 // }
-
