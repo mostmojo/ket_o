b9cb6f37514dddf8ebde273a29f98a746436458e
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-02-21 13:34:36 +0200

    Timer ES5 to ES6 conversion

--- index.js
+++ index.js
@@ -25,23 +25,24 @@
 // /* ----------- Timer ----------- */
 
 function timerBoot(duration, display) {
-    var timer = duration,
-        minutes, seconds;
-    setInterval(function() {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+	let timer = duration;
+	let minutes;
+	let seconds;
+	setInterval(() => {
+			minutes = parseInt(timer / 60, 10);
+			seconds = parseInt(timer % 60, 10);
+			minutes = minutes < 10 ? `0${minutes}` : minutes;
+			seconds = seconds < 10 ? `0${seconds}` : seconds;
+			display.textContent = `${minutes}:${seconds}`;
+			if (--timer < 0) {
+					timer = duration;
+			}
+	}, 1000);
 }
-window.onload = function() {
-    var tenMinutes = 60 * 10,
-        display = document.querySelector('#countdown');
-    timerBoot(tenMinutes, display);
+window.onload = () => {
+	const tenMinutes = 60 * 10;
+	const display = document.querySelector('#countdown');
+	timerBoot(tenMinutes, display);
 };
 
 // /* ---------- Country state -----------*/
